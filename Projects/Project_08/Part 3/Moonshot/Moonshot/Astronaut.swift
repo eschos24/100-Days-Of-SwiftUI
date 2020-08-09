35507f640fec704599a5f85418a6e5f3f30d6749
Eric Schofield <eric.w.schofield@gmail.com>
2020-08-08 20:57:10 -0600

    Day 41 - Project 8, Part 3 (Moonshot)

---
+++ Projects/Project_08/Part 3/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Eric Schofield on 8/5/20.
+//  Copyright © 2020 Eric Schofield. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
